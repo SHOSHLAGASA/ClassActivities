af39777a6c278d30531206e8d0d85ee160068585
shoshana lagasa <shoshanalagasa850@gmail.com>
2021-11-15 00:50:20 -0600

    math operators

---
+++ 2.3.1-MathOperators.js
@@ -0,0 +1,16 @@
+//console.log(10 + 5);
+//console.log (15)
+//console.log(12 - 3);
+//console.log(15 / 2);
+//console.log(4 * 5);
+// console.log(2 ** 3);
+
+// adding/concatenating strings
+//console.log("10" + "5");
+//console.log("Hello " + "world");
+
+// modulo (remainder)
+//console.log(4 % 2);
+//console.log(5 % 2);
+//console.log(6 % 2);
+//console.log(7 % 2);
